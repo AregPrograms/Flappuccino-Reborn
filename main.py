39b7b71765d795d865027c86c1f52d3d2b0fb48c
Jase <108029829+Jased-0001@users.noreply.github.com>
2022-11-29 20:04:42 -0500

    add "freeUpgrades" | difficulty curve | mod menu can now be opened with ENTER

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ from modmenu import ModMenu
 
 def main():
     pygame.init()
+
     # set the display
     DISPLAY=pygame.display.set_mode((640,480),0,32)
     pygame.display.set_caption('flappuccino reborn')
     pygame.display.set_icon(Bean().sprite)
+
+    # asset loading
+
     # get fonts
     font = pygame.font.Font('data/fonts/font.otf', 100)
     font_small = pygame.font.Font('data/fonts/font.otf', 32)
     font_title_desc = pygame.font.Font('data/fonts/font.otf', 16)
     font_20 = pygame.font.Font('data/fonts/font.otf', 20)
+
     # get some images
     shop = pygame.image.load('data/gfx/shop.png')
     shop_bg = pygame.image.load('data/gfx/shop_bg.png')
@@ -29,18 +34,24 @@ def main():
     title_bg = pygame.image.load('data/gfx/bg.png')
     title_bg.fill((255, 30.599999999999998, 0.0), special_flags=pygame.BLEND_ADD)
     shadow = pygame.image.load('data/gfx/shadow.png')
+
     # get sounds
     flapfx = pygame.mixer.Sound("data/sfx/flap.wav")
     upgradefx = pygame.mixer.Sound("data/sfx/upgrade.wav")
     beanfx = pygame.mixer.Sound("data/sfx/bean.wav")
     deadfx = pygame.mixer.Sound("data/sfx/dead.wav")
     cantAffordfx = pygame.mixer.Sound("data/sfx/cantafford.wav")
+
+
+
     # colors
     WHITE=(255,255,255) # constant
+
     # variables
     rotOffset = -5
     clock = pygame.time.Clock()
     modMenu = ModMenu()
+    showModMenu = None
 
     # creating a new object player
     player = Player()
@@ -52,11 +63,11 @@ def main():
 
     # now simply loading images based off of indexes in the list
     buttons[0].typeIndicatorSprite = pygame.image.load('data/gfx/flap_indicator.png')
-    buttons[0].price = 1   
+    buttons[0].price = 5
     buttons[1].typeIndicatorSprite = pygame.image.load('data/gfx/speed_indicator.png')
-    buttons[1].price = 1 
+    buttons[1].price = 5
     buttons[2].typeIndicatorSprite = pygame.image.load('data/gfx/beanup_indicator.png')
-    buttons[2].price = 1
+    buttons[2].price = 5
     
     x = 300
     
@@ -211,19 +222,24 @@ def main():
         dt = time.time() - last_time
         dt *= 60
         last_time = time.time()
+        
         # get the position of the mouse
         mouseX,mouseY = pygame.mouse.get_pos()  
+        
         # getting the keys pressed
         clicked = False
         keys = pygame.key.get_pressed()
+        
         # checking events
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 clicked = True
+                
             # if the player quits
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
+                
         # so the user clicked, and by any change the mouse's position was on the buttons
         if (clicked and checkCollisions(mouseX, mouseY, 3, 3, DISPLAY.get_width()/2 - retry_button.get_width()/2, 288, retry_button.get_width(), retry_button.get_height())):
             clicked = False
@@ -241,28 +257,39 @@ def main():
         pygame.display.update()
         pygame.time.delay(10)
 
+    showModMenu = False
+
     # the main game loop
     while True:
         dt = time.time() - last_time
         dt *= modMenu.config["speed"]
         last_time = time.time()
+
         # again, get the position
         mouseX,mouseY = pygame.mouse.get_pos()
 
         jump = False
         clicked = False
         keys = pygame.key.get_pressed()
+
         # get events
         for event in pygame.event.get():
             if event.type==pygame.KEYDOWN:
                 if event.key==K_SPACE:
                     jump = True
-                if event.key == 61:
+
+                #mod menu hotkeys
+                if event.key == 61: #+ key
                     modMenu.config["speed"]+=5
-                if event.key == 45:
+                if event.key == 45: #- key
                     modMenu.config["speed"]-=5
+                
+                if event.key == 1073741882: #F1
+                    modMenu.config["freeUpgrades"] = not modMenu.config["freeUpgrades"]
+                if event.key == 13: #enter
+                    showModMenu = not showModMenu
                     
-                print(event.key)
+                print(f"KEY : {event.key}")
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 clicked = True
             if clicked and mouseY < DISPLAY.get_height() - 90:
@@ -308,26 +335,29 @@ def main():
             player.velocity.x = -abs(player.velocity.x)
             player.currentSprite = player.leftSprite
             rotOffset = 5
+
         if player.position.x < 0:
             player.velocity.x = abs(player.velocity.x)
             player.currentSprite = player.rightSprite
             rotOffset = -5
+
         if jump and not dead:
             player.velocity.y = -flapForce
             pygame.mixer.Sound.play(flapfx)
+
         player.position.y += player.velocity.y*dt
         player.velocity.y = clamp(player.velocity.y + player.acceleration*dt, -99999999999, 50)
 
-        health -= 0.2*dt
+        health -= 0.2 * dt
         if health <= 0 and not dead:
             dead = True
             pygame.mixer.Sound.play(deadfx)
-            
 
         for bean in beans:
             if bean.position.y + camOffset + 90 > DISPLAY.get_height():
                 bean.position.y -= DISPLAY.get_height()*2
                 bean.position.x = random.randrange(0, DISPLAY.get_width() - bean.sprite.get_width())
+
             if (checkCollisions(player.position.x, player.position.y, player.currentSprite.get_width(), player.currentSprite.get_height(), bean.position.x, bean.position.y, bean.sprite.get_width(), bean.sprite.get_height())):
                 dead = False
                 pygame.mixer.Sound.play(beanfx)
@@ -338,21 +368,42 @@ def main():
 
         for button in buttons:
             buttonX,buttonY = 220 + (buttons.index(button)*125), 393
+
+            if modMenu.config["freeUpgrades"]: button.price = 0
+            else: pass
+
             if clicked and not dead and checkCollisions(mouseX, mouseY, 3, 3, buttonX, buttonY, button.sprite.get_width(), button.sprite.get_height()):
                 if (beanCount >= button.price):
                     pygame.mixer.Sound.play(upgradefx)
                     button.level += 1
                     beanCount -= button.price
-                    button.price = 1
-                    if (buttons.index(button) == 0):
+
+                    if modMenu.config["freeUpgrades"]: button.price = 0
+                    else: pass
+
+                    if button.price == 0 and not modMenu.config["freeUpgrades"]: button.price = 5 #reset price if player disabled free upgrades
+
+                    if buttons.index(button) == 0:
                         flapForce *= 1.5
-                    if (buttons.index(button) == 1):
+
+                        if modMenu.config["freeUpgrades"]: button.price = 0
+                        else: button.price = round(button.price * 1.5)
+
+                    if buttons.index(button) == 1:
                         player.velocity.x *= 1.5
-                    if (buttons.index(button) == 2):
+
+                        if modMenu.config["freeUpgrades"]: button.price = 0
+                        else: button.price = round(button.price * 1.5)
+
+                    if buttons.index(button) == 2:
                         beanMultiplier += 10
                         for i in range(beanMultiplier):
                             beans.append(Bean())
                             beans[-1].position.xy = random.randrange(0, DISPLAY.get_width() - bean.sprite.get_width()), player.position.y - DISPLAY.get_height() - random.randrange(0, 200)
+
+                        if modMenu.config["freeUpgrades"]: button.price = 0
+                        else: button.price = round(button.price * 1.5)
+
                 else: #player cant afford upgrade
                     pygame.mixer.Sound.play(cantAffordfx)
         
@@ -366,17 +417,21 @@ def main():
             flapForce = 3
             beanMultiplier = 5
             buttons = []
+
             for i in range(3): buttons.append(Button())
             buttons[0].typeIndicatorSprite = pygame.image.load('data/gfx/flap_indicator.png')
-            buttons[0].price = 1   
+            buttons[0].price = 5
             buttons[1].typeIndicatorSprite = pygame.image.load('data/gfx/speed_indicator.png')
-            buttons[1].price = 1 
+            buttons[1].price = 5
             buttons[2].typeIndicatorSprite = pygame.image.load('data/gfx/beanup_indicator.png')
-            buttons[2].price = 1
+            buttons[2].price = 5
             beans = []
+
             for i in range(5): beans.append(Bean())
+
             for bean in beans:
                 bean.position.xy = random.randrange(0, DISPLAY.get_width() - bean.sprite.get_width()), beans.index(bean)*-200 - player.position.y
+
             pygame.mixer.Sound.play(upgradefx)
             dead = False         
 
@@ -386,7 +441,9 @@ def main():
         bg[2].position = bg[0].position - DISPLAY.get_height()
         
         modMenu.update(font_20)
-        #DISPLAY.blit(modMenu.frame,(0, 0))
+        if showModMenu: DISPLAY.blit(modMenu.frame,(0, 0))
+        elif not showModMenu: pass
+        else: showModMenu = False # again, just in case ;)
         
         pygame.display.update()
 
--- modmenu.py
+++ modmenu.py
@@ -1,18 +1,38 @@
 import pygame
 
 class ModMenu:
-    
     def __init__(self):
         self.frame = pygame.Surface((200, 250), pygame.SRCALPHA)
         self.config = {
-            "speed":  60
+            "speed":  60,
+            "freeUpgrades": False
         }
         
     def update(self, font: pygame.font.Font):
-        self.frame.fill((0, 0, 0, 0))
+        #semi-transparent background
+        self.frame.fill((0, 0, 0, 100))
+        semiTransparent = (0, 0, 0, 100)
+
+        green = (0, 255, 0)
+        red = (255, 0, 0)
         
-        pygame.draw.rect(self.frame, (171, 145, 123), (0, 0, 200, 30), border_top_left_radius=10, border_top_right_radius=10)
+        pygame.draw.rect(self.frame, (171, 145, 123), (0, 0, 200, 100), border_top_left_radius=10, border_top_right_radius=10)
         pygame.draw.rect(self.frame, (231, 205, 183), (0, 30, 200, 220), border_bottom_left_radius=10, border_bottom_right_radius=10)
         
         title = font.render("Mod Menu", True, (255, 255, 255))
         self.frame.blit(title, (100 - (title.get_width()/2), 15 - (title.get_height()/2)))
+
+        # Free Upgrade Code
+        if self.config["freeUpgrades"]: fUp = font.render("Free Upgrades", True, green)
+        elif not self.config["freeUpgrades"]: fUp = font.render("Free Upgrades", True, red)
+        else: self.config["freeUpgrades"] = False # just in case
+
+        #render hotkey
+        fUpHotkey = font.render("F1", True, (255, 255, 255))
+
+        self.frame.blit(fUp, (100 - (fUp.get_width()/2), 50 - (fUp.get_height()/2)))
+        self.frame.blit(fUpHotkey, (100 - (fUpHotkey.get_width()/2), 70 - (fUpHotkey.get_height()/2)))
+
+
+
+        #get hotkeys
